4ff01de0a6f1748d0b67030026ee5fa333b63b26
Costa Shapiro <costa@mouldwarp.com>
2010-07-21 03:22:22 -0400

    some naughty test-oriented utilities

---
+++ ruby/to_t.rb
@@ -0,0 +1,11 @@
+
+require 'date'
+
+class String
+  # converts to DateTime by calling its #parse
+  def to_t
+    # I'm sorry there will be no spec...
+    DateTime.parse(self)
+  rescue
+  end
+end
